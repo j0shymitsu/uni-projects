233b85031549a485bc97d7459f5f4af4c3b218c4
Josh Birch <joshuabirch@protonmail.com>
2025-11-24 20:04:38 +0000

    Build basic Prototype constructor and methods

--- Prototype.cpp
+++ Prototype.cpp
@@ -9,37 +9,62 @@ using namespace std;
 Prototype::Prototype(std::string& file_name)
 {
     ifstream reader("file_name");
-    Prototype::all_cities = vector<string>();
+
+    string city;
+    all_cities = vector<string>();
+    used_cities = vector<string>();
 
     if (!reader)
     {
-
+        cout << "Error opening file." << endl;
     }
 
+    for (int i = 0; !reader.eof(); i++)
+    {
+        getline(reader, city);
+        all_cities.push_back(city);
+    }
 
-
+    reader.close();
 }
 
 // Return string containing capital city beginning with given character
-string Prototype::getCity(std::string& start_letter)
+// UNFINISHED
+string Prototype::getCity(char& start_letter)
 {
+    for (int i = 0; i < all_cities.size(); i++)
+    {
+        return all_cities[i];
+    }
 
+    return "";
 }
 
 // Check if given city is valid and unused
 bool Prototype::checkCity(std::string& city)
 {
+    for (int i = 0; i < all_cities.size(); i++)
+    {
+        if (city == all_cities[i])
+        {
+            return false;
+        }
+    }
 
+    return true;
 }
 
 // Mark given city as used
 void Prototype::markUsed(std::string& city)
 {
-
+    if (!checkCity(city))
+    {
+        used_cities.push_back(city);
+    }
 }
 
 // Mark all cities unused
 void Prototype::restart()
 {
-
+    used_cities.empty();
 }
