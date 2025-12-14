a078cc28f2a2aa9ce6c64fb480651fbe8175ceda
Josh Birch <joshuabirch@protonmail.com>
2025-12-14 16:47:21 +0000

    Implement core Competition.h file

--- Competition.h
+++ Competition.h
@@ -1,11 +1,12 @@
 #pragma once
 #include "Prototype.h"
 
-#define Competition_class Prototype 		//change to Competition once you've implemented your Competition class, defaults to Prototype
+#define Competition_class Competition		//change to Competition once you've implemented your Competition class, defaults to Prototype
 
 class Competition : public Prototype
 {
     public:
-        Competition() : Prototype("Cities.txt") {};
+        explicit Competition(std::string file_name) : Prototype(file_name) {}
+        Competition() : Prototype("Cities.txt") {}
         // std::string getCity(char start_letter) override { return Prototype::getCity(start_letter); };
 };
