918f5c54f04f20ce7cd0e60230d097bf6c9c3249
Josh Birch <joshuabirch@protonmail.com>
2025-12-18 13:45:46 +0000

    Make run method static as it doesn't access any member variables

--- Simulator.h
+++ Simulator.h
@@ -10,7 +10,7 @@ class Simulator
 {
     public:
         Simulator();
-        std::list<std::string> run(const std::string& file_name, char start_letter, int seed);   // Return words used in sim game
+        static std::list<std::string> run(const std::string& file_name, char start_letter, int seed);   // Return words used in sim game
         double batch(const std::string& file_name, int k, int seed);   // Run k sims, return time taken in s
         std::vector<std::list<std::string>> getResults();
     private:
